b1e8700b8194b48c4846bd781797054ac183f801
unknown <2020583117@qq.com>
2025-04-26 19:46:32 +0800

    feat: 集成shadcn组件库以及next-theme

---
+++ app/(site)/index.tsx
@@ -0,0 +1,13 @@
+import { ModeToggle } from "@/components/mode-toggle";
+import { Button } from "@/components/ui/button";
+
+const Site = () => {
+  return (
+    <div>
+      <Button>Click me</Button>
+      <ModeToggle />
+    </div>
+  );
+};
+
+export default Site;
